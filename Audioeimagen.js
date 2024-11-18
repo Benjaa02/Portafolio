5cd3a8730a407149fa57cec86c44a2bc29b02d3e
Benjaa02 <bbartuano2@gmail.com>
2024-11-18 13:47:39 -0500

    Actualización

--- Audioeimagen.js
+++ Audioeimagen.js
@@ -1,25 +1,27 @@
 
-    // Función para abrir el modal y mostrar la imagen en grande
+    // Abrir el modal con la imagen seleccionada
     function openModal(imageSrc) {
-        var modal = document.getElementById("myModal");
-        var modalImg = document.getElementById("imgModal");
-        var captionText = document.getElementById("caption");
-
-        modal.style.display = "block"; // Mostrar el modal
-        modalImg.src = imageSrc; // Copiar la fuente de la imagen a la imagen del modal
-        captionText.innerHTML = "Imagen ampliada"; // Leyenda
+        var modal = document.getElementById("imageModal");
+        var imgModal = document.getElementById("imgModal");
+        imgModal.src = imageSrc;
+        modal.style.display = "block";
     }
 
-    // Función para cerrar el modal
+    // Cerrar el modal
     function closeModal() {
-        var modal = document.getElementById("myModal");
-        modal.style.display = "none"; // Ocultar el modal
+        var modal = document.getElementById("imageModal");
+        modal.style.display = "none";
     }
 
-    // Función para reproducir audio cuando se hace clic en Proyecto 4
-    function playAudio() {
-        var audio = document.getElementById("audio");
-        audio.style.display = "block"; // Mostrar el control del audio si es necesario
-        audio.play(); // Reproducir el audio
-    }
+   // Reproducir el audio
+function playAudio() {
+    var audio = document.getElementById("myAudio");
+    audio.play();
+}
 
+// Detener el audio
+function stopAudio() {
+    var audio = document.getElementById("myAudio");
+    audio.pause();
+    audio.currentTime = 0;
+}
